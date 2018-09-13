22528662182d3fff4791a849f7bd4f5375c79b75
nirajan-manandhar <manandnirajan@gmail.com>
2018-09-13 15:18:59 -0700

    Implemented GCD function and test case

---
+++ gcd.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Nirajan on 2018-09-13.
+//
+
+#ifndef HELLLOCLION_GCD_HPP
+#define HELLLOCLION_GCD_HPP
+
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b.
+int gcd (const int a,const int b);
+
+#endif //HELLLOCLION_GCD_HPP
+
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include "gcd.hpp"
+
 using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     cout << "Hello Jeff" << endl;
+    cout << "GCD is: " << gcd(10, 20) << endl;
     return 0;
+}
+
+int gcd ( const int a, const int b) {
+    if (a<=0 || b<=0)
+        return 0;
+
+    if (a == b)
+        return a;
+
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
 }
